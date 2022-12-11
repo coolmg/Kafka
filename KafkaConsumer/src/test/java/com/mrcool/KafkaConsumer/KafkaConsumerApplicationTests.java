03bc2d6688a59ab23d21804cf59f6981c958392b
coolmg <gadila.mahendra@gmail.com>
2022-12-11 15:28:21 +0530

    update

--- KafkaConsumer/src/test/java/com/mrcool/KafkaConsumer/KafkaConsumerApplicationTests.java
+++ KafkaConsumer/src/test/java/com/mrcool/KafkaConsumer/KafkaConsumerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.mrcool.KafkaConsumer;
 
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.listener.MessageListenerContainer;
 
 @SpringBootTest
 class KafkaConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+
+
+
+    @Test
+    void test() {
+    }
 }
